d8124c248231187472c9815fe8893acf2e8cdfe2
Sean Connolly <seanconnolly@mail.rit.edu>
2016-07-25 11:20:14 -0400

    Added a few comments to the Semaphore section.

--- SynchronizationObjectsAndReordering/AtomicsFencesSemaphores/Semaphores.cpp
+++ SynchronizationObjectsAndReordering/AtomicsFencesSemaphores/Semaphores.cpp
@@ -3,5 +3,7 @@
 //Microsoft does not have a native version of a semaphore
 
 //Good read on semaphores:
-	//He creates his own, since Microsoft doesn't have a native implementation
+	//He creates his own/shows the idea behind a semaphore, since Microsoft doesn't have a native implementation
 	//http://austingwalters.com/multithreading-semaphores/
+		//Also, if it wasn't clear the current variable is the semaphore
+			//Remembering that what makes it a semaphore is its usage
